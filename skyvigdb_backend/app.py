7d9a7fa6e4343388590a44f54b37df7d86039adc
drmanojmph <drmanoj@gmail.com>
2026-02-16 20:59:04 +0530

    Update app.py

--- skyvigdb_backend/app.py
+++ skyvigdb_backend/app.py
@@ -597,4 +597,5 @@ def init_db():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    port = int(os.environ.get('PORT', 8080))
+    app.run(debug=False, host='0.0.0.0', port=port)
