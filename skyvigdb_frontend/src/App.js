66ef090114bd38c319f7d750d43e3d96f6f158a8
drmanojmph <drmanoj@gmail.com>
2026-02-18 14:01:22 +0530

    Update App.js

--- skyvigdb_frontend/src/App.js
+++ skyvigdb_frontend/src/App.js
@@ -1,4 +1,4 @@
-modified_app_js = '''import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
